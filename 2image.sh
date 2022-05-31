16b907ce8635ae809335cdf43efd2c7bdd13ebb3
Cybernetic1 <generic.intelligence@gmail.com>
2022-06-01 00:50:45 +0800

    cat and dog

---
+++ 2image.sh
@@ -0,0 +1,1 @@
+convert -density 300 -background white -alpha remove -trim $1 img_%02d.png
--- SVG-resize.sh
+++ SVG-resize.sh
@@ -1,1 +1,3 @@
-inkscape --batch-process --actions="select-all:all;FitCanvasToSelection;FileSave;FileClose;" $1
+# inkscape --batch-process --actions="select-all:all;FitCanvasToSelection;FileSave;FileClose;" $1
+inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
+# ***** inkscape --verb-list | grep "File"

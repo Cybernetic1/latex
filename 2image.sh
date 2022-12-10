f0cde99c708cd4ab0a1610e33e11771ccac2fb96
Cybernetic1 <generic.intelligence@gmail.com>
2022-12-10 17:19:57 +0800

    2image.sh ask options

--- 2image.sh
+++ 2image.sh
@@ -1,1 +1,7 @@
-convert -density 300 -background white -alpha remove -trim $1 img_%02d.png
+printf 'Trim margins (y/n)?'
+read answer
+if [ $answer == 'y' ] ; then
+    convert -density 300 -background white -alpha remove -trim $1 img_%02d.png
+else
+	convert -density 300 -background white -alpha remove $1 img_%02d.png
+fi

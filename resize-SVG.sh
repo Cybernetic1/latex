0381405298f83ac498993e8db5e9dc209520dfc0
Cybernetic1 <generic.intelligence@gmail.com>
2023-12-28 23:46:22 +0800

    Conways law

--- resize-SVG.sh
+++ resize-SVG.sh
@@ -1,3 +1,5 @@
-# inkscape --batch-process --actions="select-all:all;FitCanvasToSelection;FileSave;FileClose;" $1
-inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
+inkscape --actions="select-all:all; fit-canvas-to-selection; export-overwrite;" $1
+
+
+# inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
 # ***** inkscape --verb-list | grep "File"

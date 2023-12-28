1cfbf23556a40222e937700e26099e5b8bf35716
Cybernetic1 <generic.intelligence@gmail.com>
2023-12-29 00:09:47 +0800

    resize-SVG.sh

--- resize-SVG.sh
+++ resize-SVG.sh
@@ -5,7 +5,7 @@ if [[ "$INK_VERSION" == "Inkscape 1.1"* ]]; then
 	inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
 fi
 
-if [[ "$INK_VERSION" == "Inkscape 1.3"* ]]; then
+if [[ "$INK_VERSION" == "Inkscape 1.3"* ]] || [[ "$INK_VERSION" == "Inkscape 1.2"* ]] ; then
 	inkscape --actions="select-all:all;fit-canvas-to-selection;export-overwrite;export-do" $1
 fi
 

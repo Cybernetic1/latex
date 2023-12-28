4a53f97ad8d61367b01ba2868dd650793ec97ba9
Cybernetic1 <generic.intelligence@gmail.com>
2023-12-28 23:24:29 +0800

    resize-SVG.sh, for version 1.3+

--- resize-SVG.sh
+++ resize-SVG.sh
@@ -1,3 +1,13 @@
-# inkscape --batch-process --actions="select-all:all;FitCanvasToSelection;FileSave;FileClose;" $1
-inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
-# ***** inkscape --verb-list | grep "File"
+INK_VERSION="$(inkscape --version)"
+echo ${INK_VERSION}
+if [[ "$INK_VERSION" == "Inkscape 1.1"* ]]; then
+	echo "GUI seems required for this version... trying..."
+	inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
+fi
+
+if [[ "$INK_VERSION" == "Inkscape 1.3"* ]]; then
+	inkscape --actions="select-all:all;fit-canvas-to-selection;export-overwrite;export-do" $1
+fi
+
+# ***** inkscape --action-list | grep "File"
+# ***** OLD: inkscape --verb-list | grep "File"

bd05ead876d723419c175359c6f685bc8e49fb10
Cybernetic1 <generic.intelligence@gmail.com>
2024-12-23 10:08:23 +0800

    resize-SVG.sh

--- resize-SVG.sh
+++ resize-SVG.sh
@@ -3,11 +3,11 @@ echo ${INK_VERSION}
 if [[ "$INK_VERSION" == "Inkscape 1.1"* ]]; then
 	echo "GUI seems required for this version... trying..."
 	inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose --verb=FileQuit $1
-fi
-
-if [[ "$INK_VERSION" == "Inkscape 1.3"* ]] || [[ "$INK_VERSION" == "Inkscape 1.2"* ]] ; then
+else
 	inkscape --actions="select-all:all;fit-canvas-to-selection;export-overwrite;export-do" $1
 fi
+
+# if [[ "$INK_VERSION" == "Inkscape 1.3"* ]] || [[ "$INK_VERSION" == "Inkscape 1.2"* ]] ; then
 
 # ***** inkscape --action-list | grep "File"
 # ***** OLD: inkscape --verb-list | grep "File"

4588396535ce5b4300ae7dc8b7cbdb8bd751ca20
Cybernetic1 <generic.intelligence@gmail.com>
2025-05-03 16:58:59 +0800

    cropPDF.sh

---
+++ humanities/cropPDF.sh
@@ -0,0 +1,3 @@
+# --bbox <left> <bottom> <right> <top>
+# use --verbose to see bbox parameters
+pdfcrop --bbox '108 92 502 660' --margins '5 5 5 5' --no-clip Solidarity-white-paper-en.pdf Solidarity-white-paper-EN.pdf
